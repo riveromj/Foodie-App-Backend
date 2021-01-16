9f0e1539b4be917584c4728c686b9a82d5dffef6
riveromj <marigen@gmail.com>
2021-01-16 19:09:57 +0000

    creación de Recipe Model, import main y admin

--- src/models.py
+++ src/models.py
@@ -2,18 +2,27 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    title = db.Column(db.String(250),nullable=False)
+    image = db.Column(db.String(250),nullable=False)
+    ingredients = db.Column(db.String(250),nullable=False)
+    elaboration = db.Column(db.String(250),nullable=False)
+    num_comment = db.Column(db.Integer)
+    date_recipe = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    #user_id 
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    #def __repr__(self):
+      #  return '<User %r>' % self.username
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "title": self.title,
+            "image":self.image,
+            "ingredients":self.ingredients,
+            "elaboration":self.elaboration,
+            "num_comment":self.num_comment,
+            "date_recipe":self.recipe,
             # do not serialize the password, its a security breach
         }
