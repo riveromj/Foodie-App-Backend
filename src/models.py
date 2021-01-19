8c238c58b40c33bc12544f6ca06ea9acb60a6ed3
VirginiaKMartinez <hola@virginiak.es>
2021-01-19 18:50:41 +0000

    cambio en el modelo en image por db.text

--- src/models.py
+++ src/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     user_name = db.Column(db.string(80), unique=True, nullable= False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    image = db.Column(db.String(500)), nullable = True)
+    image = db.Column(db.Text), nullable = True)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __init__(self, email, password, image):
