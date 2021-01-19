8953fbbc094cdf42e09685f013da61d5ca7ef687
VirginiaKMartinez <hola@virginiak.es>
2021-01-19 19:25:33 +0000

    corregida una mayúscula el l7 y espaciados entre los iguales

--- src/models.py
+++ src/models.py
@@ -3,12 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.string(80), unique=True, nullable= False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    id = db.Column(db.Integer, primary_key = True)
+    user_name = db.Column(db.String(80), unique = True, nullable = False)
+    email = db.Column(db.String(120), unique = True, nullable = False)
+    password = db.Column(db.String(80), unique = False, nullable = False)
     image = db.Column(db.Text), nullable = True)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique = False, nullable = False)
 
     def __init__(self, email, password, image):
         self.user_name = user_name
