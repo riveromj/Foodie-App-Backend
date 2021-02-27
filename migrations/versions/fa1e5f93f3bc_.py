e001800b7219324e1c41ab2ad08ff2325266343e
riveromj <marigen@gmail.com>
2021-02-27 10:55:11 +0000

    subiendo version

--- migrations/versions/fa1e5f93f3bc_.py
+++ migrations/versions/fa1e5f93f3bc_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 8c47053cba59
+Revision ID: fa1e5f93f3bc
 Revises: 
-Create Date: 2021-02-27 09:18:53.882247
+Create Date: 2021-02-27 10:53:56.451723
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '8c47053cba59'
+revision = 'fa1e5f93f3bc'
 down_revision = None
 branch_labels = None
 depends_on = None
